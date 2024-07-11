1cc78d3d0f5714fb88b922d7e64f68129adad198
J3runo <jose-brunotj@hotmail.com>
2024-07-11 15:02:43 -0300

    11/07

--- 05-Funcões/src/exemplos.js
+++ 05-Funcões/src/exemplos.js
@@ -1,5 +1,4 @@
-import leia from "readline-sync"
-
+import leia from "readline-sync";
 
 /*function mostrar(){
     var nome = leia.question("Digite o nome\n")
@@ -22,14 +21,42 @@ function subtrair(num1,num2){
     console.log("O RESULTADO É : " + result)
 }
 subtrair(30,20)
-*/
+
 //----------------------------------------------------------
 
-function multi(num1,num2){
-    return num1 * num2
+function multi(num1, num2) {
+  return num1 * num2;
 }
 
-multi = leia.questionInt("numero\n")
-var resutMulti = multi()
+multi = leia.questionInt("numero\n");
+var resutMulti = multi();
 
-console.log(resutMulti)
+console.log(resutMulti);
+*/
+/*function parImpar(n) {
+  if (n % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var n = leia.questionInt("digite o numero\n");
+var result = parImpar(n);
+
+if (result === true) {
+  console.log("O " + n + " É PAR ");
+} else if (result === false) {
+  console.log("O " + n + " É IMPAR ");
+}
+*/
+function soma(num1, num2) {
+  return num1 * num2;
+}
+
+var num1 = leia.questionInt("numero\n");
+var num2 = leia.questionInt("numero\n");
+
+var result = soma(num1, num2);
+
+console.log("o resultado é: " + result);
